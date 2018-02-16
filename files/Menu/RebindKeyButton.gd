497d7bc9eb04e04cc30880575cac493ca4d336fa
James Beedie <jellonator00@gmail.com>
2018-02-16 16:20:27 -0500

    Removed cancel button from keybind change
    (It was causing crashes)

--- files/Menu/RebindKeyButton.gd
+++ files/Menu/RebindKeyButton.gd
@@ -4,7 +4,6 @@ export (String) var action_name
 var previous_input
 
 onready var button = $Change
-onready var cancel_button = $Cancel
 onready var label = $Label
 
 func update_self():
@@ -17,9 +16,6 @@ func update_config(event):
 	UserConfig.set_keybind(action_name, event);
 
 func _input(event):
-	yield(get_tree(), "idle_frame")
-	if cancel_button.has_focus():
-		return
 	if button.is_pressed():
 		var e = UserConfig.check_input_valid(event)
 		if e != null:
@@ -30,28 +26,15 @@ func _input(event):
 
 func _physics_process(delta):
 	button.set_disabled(button.is_pressed())
-	if button.is_pressed():
-		cancel_button.show()
-	else:
-		cancel_button.hide()
-
-func cancel():
-	UserConfig.set_keybind(action_name, previous_input);
-	button.set_pressed(false)
-	update_self();
 
 func _ready():
 	update_self();
 	label.set_text(UserConfig.get_action_name(action_name))
-	cancel_button.hide()
 	previous_input = UserConfig.get_keybind(action_name);
 
 func _on_ChangeButton_released():
 	button.set_disabled(false)
 	update_self()
-
-func _on_Cancel_pressed():
-	cancel()
 
 func _on_Change_pressed():
 	button.set_disabled(true)
